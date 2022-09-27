c58e1a27a3121d66547cf148a5c7cf244adf6fe2
Jenna Otten <jenna.otten@gmail.com>
2022-09-26 18:02:27 -0700

    Completed Lab

--- ratings.py
+++ ratings.py
@@ -1,31 +1,37 @@
 ratings_file = open('scores.txt')
-
 """Restaurant rating lister."""
 
 def create_dictionary(file):
+    """Reads scores file and returns a dictionary"""
 
-    sorted_ratings = {}
+    restaurant_scores = {}
 
     for line in file:
         line = line.strip()
         restaurant, score = line.split(":")
-        sorted_ratings[restaurant] = int(score)
+        restaurant_scores[restaurant] = int(score)
         
-    return sorted_ratings
+    return restaurant_scores
 
-dictionary_created = create_dictionary(ratings_file)
+    #dictionary_created = create_dictionary(ratings_file)
 
-def add_user_ratings():
+def add_user_ratings(ratings):
+    """Add a user restaurant and rating."""
     restaurant_name = input("Enter a restaurant name. ")
-    restaurant_score = input("How would you rate it? ")
-    dictionary_created[restaurant_name] = restaurant_score
+    restaurant_rating = input("How would you rate it? ")
+    ratings[restaurant_name] = restaurant_rating
 
 def sort_ratings(file):
+    """Print out restaurants and ratings sorted by alphabetical order."""
     
     dictionary_created
 
     for restaurant, score in sorted(file.items()):
-        print(f"{restaurant}: {score}")
+        print(f"Restaurant: {restaurant}, Rating: {score}")
 
-sort_ratings(dictionary_created)
-        
+
+dictionary_created = create_dictionary(ratings_file)
+
+add_user_ratings(dictionary_created)
+
+sort_ratings(dictionary_created)  
